8b87bb80413a76dfc3fe16157d0e3d68732aef20
bobk <ramentokyo@gmail.com>
2019-09-20 12:58:13 -0400

    additional temp fix for SQLCMD

--- sql/sqlin.cmd
+++ sql/sqlin.cmd
@@ -1,1 +1,3 @@
-sqlcmd.exe --connection-string %AZUREJIRAMETRICS_CONNSTRING% -i .\sqlout.sql
+rem TODO: change this to work with an ODBC-style connection string, how?
+set SQLOUT=.\sqlout.sql
+sqlcmd.exe --connection-string %AZUREJIRAMETRICS_CONNSTRING% -i %SQLOUT%
